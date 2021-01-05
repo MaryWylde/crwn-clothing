707097453861b5433d37914ac60873e7ed0bedd2
Mary Khachatryan <marykhachatryan13@gmail.com>
2021-01-05 15:59:46 +0400

    first one

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+   
+       <HomePage />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../directory/directory.component';
+import './homepage.styles.scss';
+
+// function because we dont really need lifecycle components here
+
+const HomePage = () => (
+<div className="homepage">
+ <Directory /> 
+</div>
+);
+
+export default HomePage;
+// beacuse we are not gonna export anything else from our homepage
